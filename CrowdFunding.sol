e8f0042c97971b098374cb72ea1517037450b407
Yash Jagtap <jagtapyash2512@gmail.com>
2022-03-21 20:52:40 +0530

    Update CrowdFunding.sol

--- CrowdFunding.sol
+++ CrowdFunding.sol
@@ -1,2 +1,11 @@
 //SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.5.0 < 0.9.0;
+
+contract CrowdFunding{
+    mapping(address=>uint) public contributors; 
+    address public manager; 
+    uint public minimumContribution;
+    uint public deadline;
+    uint public target;
+    uint public raisedAmount;
+    uint public noOfContributors;
